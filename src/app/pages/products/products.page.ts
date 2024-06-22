0aa9fe88c11034fc0533e6df635502be01e9bc6d
micha <micha2mc@gmail.com>
2024-06-22 11:53:13 +0200

    [Híbridas Inicial]first commit

---
+++ src/app/pages/products/products.page.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.page.html',
+  styleUrls: ['./products.page.scss'],
+})
+export class ProductsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
